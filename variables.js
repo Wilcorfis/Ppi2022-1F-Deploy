cc34e30c8705fb219536fee14d2554575afd0521
Wilcorfis <wilmerandres505@gmail.com>
2022-11-15 18:30:03 -0500

    variable

--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
 const variable="https://pedidoambrosia.herokuapp.com"
+
+
+function ExportToExcel(type, fn, dl) {
+    var elt = document.getElementById('ctable');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+}
+function ExportToExcel2(type, fn, dl) {
+    var elt = document.getElementById('ctable2');
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+    return dl ?
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+}
